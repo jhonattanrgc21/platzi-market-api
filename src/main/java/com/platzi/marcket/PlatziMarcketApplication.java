ab4940ed1eb2a07960bd9f8e31c2e6845006f498
Jhonattan Garcia <jgarciac@intelix.biz>
2023-07-12 12:06:00 -0400

    Estructura base del proyecto

---
+++ src/main/java/com/platzi/marcket/PlatziMarcketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.marcket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarcketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarcketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/marcket/PlatziMarcketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.marcket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarcketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
